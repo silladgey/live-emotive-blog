8b87ccdb151128b3e222d1f6d850578b1efbfbf8
Richárd Szilágyi <patrik.szilagyi@mycit.ie>
2022-11-29 17:51:08 +0000

    Configure Composer manager

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'szpatrichard/live-emotive-blog',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2e3a16bca4ecc63723a1005406becc9ff997680c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'szpatrichard/live-emotive-blog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2e3a16bca4ecc63723a1005406becc9ff997680c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
